1b214f383cd0ec68c542fd561b6e37b5da1fd199
Min <catomania@users.noreply.github.com>
2015-10-11 16:02:34 -0700

    Create py-set-intersection-operation.py

---
+++ HackerRank_Puzzles/py-set-intersection-operation.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/py-set-intersection-operation
+
+english_subs = raw_input() # I don't even use this...
+
+
+# turn english roll into integers
+english_roll_set = set(map(int, raw_input().split(" ")))
+
+
+french_subs = raw_input() # I don't even use this...
+
+# turn french roll into integers
+french_roll_set = set(map(int, raw_input().split(" ")))
+
+
+# return back roll of students who have at least one subscription
+print len(english_roll_set.intersection(french_roll_set))
