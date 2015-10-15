df16a7733e6a5fbe6a20170eaeb3cf68d48a0397
Min <catomania@users.noreply.github.com>
2015-10-14 21:39:43 -0700

    Create sudoku_scratch.py

---
+++ scratch/sudoku_scratch.py
@@ -0,0 +1,8 @@
+test_list_of_lists = [[1], [2], [3]]
+
+print test_list_of_lists[1][0] # returns 2, so '1st' in our list of lists
+print test_list_of_lists[2][0] #returns 3
+
+test2_list_of_lists = [[[1], [2]], [[3], [4]], [[5], [6]]]
+print test2_list_of_lists[0][1] # gives back 2
+print test2_list_of_lists[1][1] # gives back 4
